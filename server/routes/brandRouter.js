bc68d25f910c4b8208084386e99852d0f0b4400a
Mikhail Gorbadey <gormisha05@yandex.ru>
2022-09-07 15:46:54 +0300

    server part done

--- server/routes/brandRouter.js
+++ server/routes/brandRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router()
 const brandController = require('../controllers/brandController')
+const checkRole = require('../middlewares/checkRoleMiddleware')
 
-router.get('/',brandController.getAll)
-router.post('/',brandController.create)
+router.get('/', brandController.getAll)
+router.post('/', checkRole('ADMIN'), brandController.create)
 
 module.exports = router
